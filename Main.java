628fed76e8c6ce4568afb3a7698575792b5df13d
unknown <jasper-dehmel@web.de>
2023-05-10 17:04:42 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -7,27 +7,18 @@
  */
 public class Main
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Main
-     */
-    public Main()
+    public static void main (String args[])
     {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+        int summe = 0;
+        for(int i = 1; i <= 10; i++)
+        {
+            summe = summe + i;
+        }
+        
+        int mult = 1;
+        for(int i = 2; i <= 10; ++i)
+        {
+            mult = mult * i;
+        }
     }
 }
