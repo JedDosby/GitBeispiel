29f9e62adff0ade55bd78a6d5306e6eb94f3c68b
unknown <jasper-dehmel@web.de>
2023-05-10 17:20:36 +0200

    Variable obreGrnze mit dem Wert 20 für die obereGrenze

--- Main.java
+++ Main.java
@@ -9,14 +9,15 @@ public class Main
 {
     public static void main (String args[])
     {
+        int obreGrnze = 20;
         int summe = 0;
-        for(int i = 1; i <= 10; i++)
+        for(int i = 1; i <= obreGrnze; i++)
         {
             summe = summe + i;
         }
         
         int mult = 1;
-        for(int i = 2; i <= 10; ++i)
+        for(int i = 2; i <= obreGrnze; ++i)
         {
             mult *= i;
         }
